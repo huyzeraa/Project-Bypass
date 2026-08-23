67a8115db9e3092eca4b42acaf43d4094acb2d65
Zeraa <huyzeraadevs@gmail.com>
2026-08-23 22:15:43 +0700

    Create Auto_Faucet.user.js

---
+++ Faucet/Auto_Faucet.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name                         CryptoLinkForEarn Auto Faucet Tool
+// @name:vi                      CryptoLinkForEarn Auto Faucet Tool
+// @name:en                      CryptoLinkForEarn Auto Faucet Tool
+// @name:hi                      CryptoLinkForEarn Auto Faucet Tool
+// @name:id                      CryptoLinkForEarn Auto Faucet Tool
+// @name:fil                     CryptoLinkForEarn Auto Faucet Tool
+// @name:pt-BR                   CryptoLinkForEarn Auto Faucet Tool
+// @name:ru                      CryptoLinkForEarn Auto Faucet Tool
+// @name:tr                      CryptoLinkForEarn Auto Faucet Tool
+// @name:es                      CryptoLinkForEarn Auto Faucet Tool
+// @name:uk                      CryptoLinkForEarn Auto Faucet Tool
+// @name:th                      CryptoLinkForEarn Auto Faucet Tool
+// @name:ur                      CryptoLinkForEarn Auto Faucet Tool
+// @name:ar                      CryptoLinkForEarn Auto Faucet Tool
+// @name:zh-CN                   CryptoLinkForEarn 自动水龙头工具
+// @name:de                      CryptoLinkForEarn Auto Faucet Tool
+// @name:fr                      CryptoLinkForEarn Auto Faucet Tool
+// @name:ja                      CryptoLinkForEarn 自動フォーセットツール
+// @name:ko                      CryptoLinkForEarn 자동 포셋 툴
+// @description                  Auto claim faucet, remove ads, modern UI, Free. - Home for devs
+// @description:vi               Tự động nhận faucet, xóa quảng cáo, giao diện hiện đại, Miễn phí. - Home for devs
+// @description:en               Auto claim faucet, remove ads, modern UI, Free. - Home for devs
+// @description:hi               ऑटो क्लेम फॉसेट, विज्ञापन हटाएं, आधुनिक UI, मुफ़्त। - Home for devs
+// @description:id               Klaim faucet otomatis, hapus iklan, UI modern, Gratis. - Home for devs
+// @description:fil              Awtomatikong claim sa faucet, alisin ang mga ad, modernong UI, Libre. - Home for devs
+// @description:pt-BR            Reivindicação automática de faucet, remover anúncios, UI moderna, Grátis. - Home for devs
+// @description:ru               Авто-клейм крана, удаление рекламы, современный интерфейс, Бесплатно. - Home for devs
+// @description:tr               Otomatik musluk talebi, reklamları kaldır, modern UI, Ücretsiz. - Home for devs
+// @description:es               Reclamo automático de faucet, eliminar anuncios, UI moderna, Gratis. - Home for devs
+// @description:uk               Автоматичний збір з крана, видалення реклами, сучасний UI, Безкоштовно. - Home for devs
+// @description:th               เคลม faucet อัตโนมัติ, ลบโฆษณา, UI ทันสมัย, ฟรี - Home for devs
+// @description:ur               خودکار کلیم فوست، اشتہارات ہٹائیں، جدید UI، مفت۔ - Home for devs
+// @description:ar               مطالبة تلقائية بالحنفية، إزالة الإعلانات، واجهة حديثة، مجاني. - Home for devs
+// @description:zh-CN            自动领取水龙头，去除广告，现代化UI，免费。 - Home for devs
+// @description:de               Automatischer Faucet-Claim, Werbung entfernen, modernes UI, Kostenlos. - Home for devs
+// @description:fr               Réclamation automatique de faucet, suppression des pubs, interface moderne, Gratuit. - Home for devs
+// @description:ja               フォーセット自動クレーム、広告削除、モダンUI、無料。 - Home for devs
+// @description:ko               포셋 자동 클레임, 광고 제거, 모던 UI, 무료. - Home for devs
+// @namespace                    https://github.com/huyzeraa
+// @version                      2026.38.0
+// @author                       Zeraa - Verrni & mushr (vxrn team)
+// @copyright                    2026, vxrn team
+// @license                      Proprietary / All Rights Reserved
+// @homepageURL                  https://github.com/huyzeraa/Project-Bypass
+// @supportURL                   https://discord.com/users/1249558116761342016
+// @source                       https://github.com/huyzeraa/Project-Bypass/tree/main/Faucet/
+// @updateURL                    https://raw.githubusercontent.com/huyzeraa/Project-Bypass/main/Faucet/Auto_Faucet.js
+// @downloadURL                  https://raw.githubusercontent.com/huyzeraa/Project-Bypass/main/Faucet/Auto_Faucet.js
+// @require                      https://raw.githubusercontent.com/huyzeraa/Project-Bypass/refs/heads/main/Faucet/Auto_Faucet.js
+// @contributionURL              https://bscscan.com/address/0xbb6c7F668B270C1c569B6a1b9C2Cfa2cfCDc7c49
+// @icon                         https://raw.githubusercontent.com/huyzeraa/Project-Bypass/refs/heads/main/Faucet/Zeraa.png
+// @icon64                       https://raw.githubusercontent.com/huyzeraa/Project-Bypass/refs/heads/main/Faucet/Zeraa.png
+// @defaulticon                  https://raw.githubusercontent.com/huyzeraa/Project-Bypass/refs/heads/main/Faucet/Zeraa.png
+// @tag                          faucet
+// @tag                          crypto
+// @tag                          autoclaim
+// @tag                          cryptolinkforearn
+// @compatible                   chrome Tested on Chrome / Kiwi Browser with Tampermonkey
+// @compatible                   firefox Tested on Firefox with Tampermonkey / Violentmonkey
+// @compatible                   edge Tested on Edge
+// @compatible                   brave Tested on Brave Browser
+// @compatible                   opera Tested on Opera
+// @match                        https://cryptolinkforearn.com/faucet*
+// @exclude                      https://cryptolinkforearn.com/cdn-cgi/*
+// @grant                        none
+// @run-at                       document-start
+// ==/UserScript==
