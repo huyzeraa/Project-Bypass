385969d1d48f234f2a4c50a3afd9e49b1cd0a423
Zeraa <huyzeraadevs@gmail.com>
2026-08-30 21:58:52 +0700

    Create LaymaPubcrypto.user.js

---
+++ LaymaPubcrypto.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name            Redirect Blocker - LayMa Pubcrypto
+// @namespace       https://github.com/Zeraa/Bypass-Project
+// @version         6.0.0
+// @description     Redirect Blocker for Layma (pubcrypto)
+// @author          Zeraa - by vxrn team
+// @contributor     Pubcrypto
+// @homepageURL     https://xhider.xyz/raw/Zeraa/Bypass-Project/pubcrypto-layma.js
+// @supportURL      https://github.com/Zeraa/Bypass-Project/issues
+// @require         https://xhider.xyz/raw/Zeraa/Bypass-Project/pubcrypto-layma.js
+// @updateURL       https://xhider.xyz/raw/Zeraa/Bypass-Project/pubcrypto-layma.js
+// @downloadURL     https://xhider.xyz/raw/Zeraa/Bypass-Project/pubcrypto-layma.js
+// @contributionURL https://bscscan.com/address/0xbb6c7F668B270C1c569B6a1b9C2Cfa2cfCDc7c49
+// @icon            https://avatars.githubusercontent.com/u/309335833?v=4
+// @match           *://anonlink.co/*
+// @grant           unsafeWindow
+// @grant           GM_setClipboard
+// @grant           GM_getValue
+// @grant           GM_setValue
+// @grant           GM_xmlhttpRequest
+// @grant           GM_notification
+// @run-at          document-start
+// @noframes        false
+// @license         PROPRIETARY
+// @compatible      chrome
+// @compatible      edge
+// @compatible      firefox
+// @compatible      opera
+// @compatible      kiwi
+// ==/UserScript==
