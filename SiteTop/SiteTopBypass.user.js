488f4ec09f983290773ac7d7fccfd78d8e0d5d2a
Zeraa <huyzeraadevs@gmail.com>
2026-09-05 21:56:00 +0700

    Create SiteTopBypass.user.js

---
+++ SiteTop/SiteTopBypass.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         SiteTop Bypass
+// @version      1.1
+// @author       huyzeraa
+// @match        https://sitetop.net/*
+// @grant        GM_xmlhttpRequest
+// @connect      sitetop.net
+// @run-at       document-idle
+// @require      https://raw.githubusercontent.com/huyzeraa/Project-Bypass/refs/heads/main/SiteTop/Bypass.js
+// ==/UserScript==
